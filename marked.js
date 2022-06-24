a2c527078dc7400f77687883bbbe8062102f9bb8
jeong jun <jungjunjenon3047@gmail.com>
2022-06-25 06:55:56 +0900

    npm update

--- marked.js
+++ marked.js
@@ -1,13 +1,55 @@
-import { marked } from "marked";
+const path = require("path");
+const fs = require("fs");
 
-const body = `
-# h1 header
-Loren ipsum **test** area
-[link](https://blog.itcode.dev)
-![image](https://blog.itcode.dev/img/)
-<span class="red">native html tag</span>
-`;
+const directoryPath = path.join(__dirname, "post");
 
-const result = marked(body);
+const contentFiles = fs.readdirSync(directoryPath);
 
-console.log(result.toString());
+const hljs = require("highlight.js");
+
+const md = require("markdown-it")({
+  html: false,
+  xhtmlOut: false,
+  breaks: false,
+  langPrefix: "language-",
+  linkify: true,
+  typographer: true,
+  quotes: "“”‘’",
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return (
+          '<pre class="hljs"><code>' +
+          hljs.highlight(lang, str, true).value +
+          "</code></pre>"
+        );
+      } catch (__) {}
+    }
+
+    return (
+      '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + "</code></pre>"
+    );
+  }
+});
+
+
+
+contentFiles.map(file => {
+    const body = fs.readFileSync(`./post/${file}`, "utf8");
+    const convertedBody = md.render(body); // 이 부분을 추가해주세요!
+    console.log(convertedBody);  
+});
+  
+
+// list_format.html파일 읽기
+const listHtmlFormat = fs.readFileSync("./templates/list_format.html", "utf8");
+
+// index.html파일 생성 / 파일 목록 렌더
+const listContent = ejs.render(listHtmlFormat, {
+  lists: deployFiles
+});
+const listHtml = ejs.render(layoutHtmlFormat, {
+  content: listContent
+});
+
+fs.writeFileSync("./index.html", listHtml);
